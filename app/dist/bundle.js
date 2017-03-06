2db49aa2e94f82b3979f9efc8d9a1877f46b6664
jaouad <jaouadtelmoudy@gmail.com>
2017-03-06 12:48:14 +0100

    Branche1 : Ajout du service & templating

--- app/dist/bundle.js
+++ app/dist/bundle.js
@@ -161,7 +161,7 @@ var ProduitService = exports.ProduitService = function () {
     _createClass(ProduitService, [{
         key: 'getAllProduit',
         value: function getAllProduit() {
-            return [{ reference: '11111', libelle: 'Lait', prix: '1' }, { reference: '11112', libelle: 'Viande', prix: '3' }, { reference: '11113', libelle: 'Poulet', prix: '3' }, { reference: '11114', libelle: 'Dessert', prix: '2' }];
+            return [{ reference: '11111', libelle: 'Samsung S7', prix: '500', photo: 'app/img-product/11111.jpg', description: 'Les Galaxy S7 et S7 edge repoussent les limites en associant le meilleur du design et des fonctionnalités. Nous avons créé une expérience encore jamais vue sur smartphone, mais dont vous ne pourrez bientôt plus vous passer.' }, { reference: '11112', libelle: 'Samsung S6', prix: '300', photo: 'app/img-product/11112.jpg', description: 'Compatible 4G (débit jusquà 150 Mbit/s),Écran 5 pouces HD IPS et On-Cell,Appareil photo 8 Mégapixels et 5 Mégapixels en frontal,Silhouette fine et élégante' }, { reference: '11113', libelle: 'Samsung S5', prix: '260', photo: 'app/img-product/11113.jpg', description: 'Compatibilité LTE 4G Oui,Taille écran 5.1, Système d exploitation Android,Mémoire interne 16 Go' }];
         }
     }], [{
         key: 'getInstance',
@@ -191,7 +191,15 @@ var _produit = __webpack_require__(0);
 __webpack_require__(6);
 
 var p = new _produit.Produit("111111", "Lait", "1");
-console.log(p.service.getAllProduit());
+var result = p.service.getAllProduit();
+
+var _html = "";
+for (var i in result) {
+    console.log(result[i].prix);
+    _html += '<div class="col-sm-4 col-lg-4 col-md-4">\n                <div class="thumbnail">\n                        <img src="' + result[i].photo + '" alt="">\n                        <div class="caption">\n                        <h4 class="pull-right">' + result[i].prix + ' \u20AC</h4>\n                        <h4><a href="#">' + result[i].libelle + '</a></h4>\n                    <p>' + result[i].description + '.</p>\n                </div>\n                    <div class="ratings">\n                        <p class="pull-right">15 reviews</p>\n                        <p>\n                            <span class="glyphicon glyphicon-star"></span>\n                            <span class="glyphicon glyphicon-star"></span>\n                            <span class="glyphicon glyphicon-star"></span>\n                            <span class="glyphicon glyphicon-star"></span>\n                            <span class="glyphicon glyphicon-star"></span>\n                        </p>\n                    </div>\n                </div>\n          </div>\n        ';
+}
+
+document.getElementById("contenu").innerHTML = _html;
 
 /***/ }),
 /* 6 */
--- app/produit/produit.service.js
+++ app/produit/produit.service.js
@@ -9,9 +9,10 @@ export class ProduitService {
             return instance;
     }
     getAllProduit() {
-        return [{reference: '11111',libelle: 'Lait', prix:'1'},
-                {reference:'11112', libelle:'Viande', prix:'3'},
-                {reference:'11113',libelle:'Poulet', prix:'3'},
-                {reference:'11114', libelle:'Dessert', prix:'2'}];
+        return [
+            {reference:'11111', libelle: 'Samsung S7', prix:'500',photo:'app/img-product/11111.jpg',description:'Les Galaxy S7 et S7 edge repoussent les limites en associant le meilleur du design et des fonctionnalités. Nous avons créé une expérience encore jamais vue sur smartphone, mais dont vous ne pourrez bientôt plus vous passer.'},
+            {reference:'11112', libelle: 'Samsung S6', prix:'300',photo:'app/img-product/11112.jpg',description:'Compatible 4G (débit jusquà 150 Mbit/s),Écran 5 pouces HD IPS et On-Cell,Appareil photo 8 Mégapixels et 5 Mégapixels en frontal,Silhouette fine et élégante'},
+            {reference:'11113', libelle: 'Samsung S5', prix:'260',photo:'app/img-product/11113.jpg',description:'Compatibilité LTE 4G Oui,Taille écran 5.1, Système d exploitation Android,Mémoire interne 16 Go'}
+        ];
     }
 }
